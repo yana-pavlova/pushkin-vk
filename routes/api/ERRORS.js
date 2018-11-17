290035a3b10b500b427510ccf275029fe43489be
Yana <yana.filfak@gmail.com>
2018-11-17 14:38:34 +0300

    Update ERRORS.js

--- routes/api/ERRORS.js
+++ routes/api/ERRORS.js
@@ -2,7 +2,7 @@ const ERRORS = {
     INNER_ERROR: 'Что-то пошло не так.',
     USER_ERROR: 'Неверный пользователь',
     NOT_FOUND: 'Не найдено.',
-     
+    TEMPORARY_UNAVAILABLE: 'Временно недоступно', // HACK!
 }
 
 module.exports = ERRORS;
